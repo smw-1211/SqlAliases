2506ba98d22ea9be3e76b786cc29f28db8dc719c
smw-1211 <wattamwarsoham2000@gmail.com>
2024-05-17 10:42:07 +0530

    demo of aggregation function - count

---
+++ Aggregations.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE prod;
+use prod;
+show tables;
+
+##Aggregation Functions
+
+## Creating a table and inserting dummy records
+create table products( prod_id int primary key, prod_name varchar(20), price int);
+insert into products values (1,'DVD player', 2000);
+insert into products values(2,'video game', 1000);
+insert into products values(3, 'Lawn chair',500);
+insert into products values(4,'Toy',300);
+insert into products values(5,'Cadbury',50);
+insert into products values(6,'Chocolate',30);
+insert into products values (7,'Coffee Table',3000);
+
+select * from products;
+
+----------------------------------------------------------------------------------
+
+##Aggregation Functions - Count
+
+SELECT COUNT(*) FROM products;
+
+## COUNT() with WHERE
+SELECT COUNT(*) FROM products; WHERE price>=20;
+SELECT COUNT(DISTINCT prod_id) FROM products;
+
+## COUNT() with GROUP BY
+SELECT prod_id, COUNT(*) FROM products GROUP BY prod_id;
+SELECT prod_id, COUNT(*) FROM products GROUP BY prod_id
+HAVING COUNT(*)>2;
+
